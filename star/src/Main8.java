cb2ee788b04f9ebcd739e42812112cfbc2e40c54
ljhee92 <weilainib7@gmail.com>
2023-12-15 21:25:47 +0900

    별찍기 문제풀이

---
+++ star/src/Main8.java
@@ -0,0 +1,16 @@
+
+public class Main8 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=i+4; j++) {
+                if (j-i<=4 && i+j>=6) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
